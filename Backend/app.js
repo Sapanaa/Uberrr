b397f55a236532360decdd94280af5d6f25920b7
a53232 <a53232@alunos.ipb.pt>
2024-11-27 19:23:30 +0000

    Backend files

---
+++ Backend/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+
+module.exports = app
